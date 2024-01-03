680a0a7815a0ca0362573024c3758e3a28389767
FaizulOsman <faizulosmanrabby@gmail.com>
2024-01-03 19:34:10 +0600

    solve: find largest/lowest element from the array

--- index.js
+++ index.js
@@ -14,3 +14,25 @@ console.log(uniqueArray1);
 const uniqueSet = new Set(arr);
 const uniqueArray2 = Array.from(uniqueSet);
 console.log(uniqueArray2);
+
+/* ============ How to find out the largest/lowest elements from this array? ============ */
+// Solution 1:
+let largestElement1 = arr[0];
+let lowestElement1 = arr[0];
+
+for (let i = 1; i < arr.length; i++) {
+  if (arr[i] > largestElement1) {
+    largestElement1 = arr[i];
+  } else if (arr[i] < lowestElement1) {
+    lowestElement1 = arr[i];
+  }
+}
+console.log("Largest Element:", largestElement1);
+console.log("Lowest Element:", lowestElement1);
+
+// Solution2:
+const largestElement2 = Math.max(...arr);
+const lowestElement = Math.min(...arr);
+
+console.log("Largest Element:", largestElement2);
+console.log("Lowest Element:", lowestElement);
